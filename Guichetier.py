50ceb903c339600c015249cb42d7c5c24c30bd69
Gamator22 <56966109+Gamator22@users.noreply.github.com>
2019-11-11 13:46:58 +0100

    Guichetier.py

---
+++ Guichetier.py
@@ -0,0 +1,24 @@
+from Compte import Compte
+ 
+class Guichetier(Compte): #Sous-Classe Guichetier ayant comme classe mere Personne
+     montantv = input("Saisir le Montant de versement\n")
+    # Notre méthode constructeur
+
+     def __init__(self,solde): 
+
+         #Constructeur de notre classe
+      
+      def affSolde(self):
+        print("Solde disponible est de {0} FCFA : Protected".format(solde))
+
+     def versement(self,compte,montantv):
+         self.montantv=montantv
+        # Un montant de la somme que vous entrez au clavier va etre retirer
+         print("Versement de {0} : Protected".format(compte.montantv))
+         self.affSolde(compte)
+    
+     def retrait(self,Compte,affSolde):
+        """ ici, un test va etre effectuer dans la classe Controlleur pour savoir si le solde disponible est suffisant """
+        # Si "OUI" ... un retrait de 7000 par exemple est effectuer
+        print("montant débité de {0} FCFA: Protected".format(soldeDispo))
+        self.affSolde()
